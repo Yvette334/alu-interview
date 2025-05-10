11a43aa29545632ed8d82528e66cd7a5838e9b08
Yvette334 <y.muhoracye@alustudent.com>
2025-05-10 20:32:25 +0000

    added

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+"""minimal operation"""
+
 
 def minOperations(n):
- 
-    if n < 2:
-        return 0
-
-    operations = 0
-    factor = 2
+    """ a function that returns minimal operation to produce
+        H n amount of times with a given n integer
+    """
+    if n <= 1:
+        return (0)
+    operation = 0
+    divisor = 2
 
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-
-    return operations
+        while n % divisor == 0:
+            operation += divisor
+            n //= divisor
+        divisor += 1
+    return operation
